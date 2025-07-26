fe8b11d3e8fbd0ea3e31d77851ed649f55091433
anniemated <annie9ma@gmail.com>
2025-07-25 23:07:06 -0400

    Revert "failed color change attempt"
    This reverts commit 610fcf52c546f0a08d671ec5ef23eb027b6f25cf.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
 class Post < ApplicationRecord
   has_one_attached :image
+end
 
 sticky_colors = [ "#fffacd", "#ffd5ec", "#ccf2ff", "#d0f8ce" ]
-
- validates :color, inclusion: { in: sticky_colors }, allow_blank: true
-end
--- db/migrate/20250726020747_addpostitcolor.rb
+++
@@ -1,5 +0,0 @@
-class Addpostitcolor < ActiveRecord::Migration[8.0]
-  def change
-    add_column :posts, :color, :string
-  end
-end
